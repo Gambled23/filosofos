bbf070fcfd74cc8ce260bd045a4e4e25303bfa19
Gambled23 <ipog71@gmail.com>
2024-04-17 09:59:23 -0600

    parar al comer 6 veces

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,19 @@ use std::thread;
 
 fn main() {
     let mut philosophers = vec![];
-    // Arc: allow multiple threads (philosophers) to access the same data
-    // Mutex: Ensure that only one philosopher can access the data at a time
     let forks = Arc::new(Mutex::new(vec![false; 5]));
     let eat_counter = Arc::new(Mutex::new(vec![0; 5]));
 
-
     for i in 0..5 {
-        // Clone the data to avoid data races
         let forks = Arc::clone(&forks);
         let eat_counter = Arc::clone(&eat_counter);
         philosophers.push(thread::spawn(move || {
             let lower = std::cmp::min(i, (i + 1) % 5);
             let higher = std::cmp::max(i, (i + 1) % 5);
-            loop {
+            while eat_counter.lock().unwrap()[i] < 6 {
                 {
-                    let mut forks = forks.lock().unwrap(); // Lock the forks, so that only one philosopher can access them
+                    let mut forks = forks.lock().unwrap();
                     if !forks[lower] && !forks[higher] {
-                        // Pick up forks and eat
                         forks[lower] = true;
                         forks[higher] = true;
 
@@ -33,7 +28,7 @@ fn main() {
                 }
                 thread::sleep(std::time::Duration::from_secs(1));
 
-                { // scope to put down forks
+                {
                     let mut forks = forks.lock().unwrap();
                     forks[lower] = false;
                     forks[higher] = false;
@@ -45,7 +40,6 @@ fn main() {
         }));
     }
 
-    // Join all all threads and wait for them to finish eating, if any thread panics, the program will panic
     for philosopher in philosophers {
         philosopher.join().unwrap();
     }
